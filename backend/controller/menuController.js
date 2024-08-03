97e081b19670e7c3272e244b39360f5c4a630f6d
farhan-jabil <farhanhasan295@gmail.com>
2024-08-03 17:01:50 +0600

    working on menu - backend

--- backend/controller/menuController.js
+++ backend/controller/menuController.js
@@ -16,3 +16,35 @@ exports.add = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getBySku = async (req, res, next) => {
+  try {
+    const data = await Menu.findOne({ foodSku: req.params.foodSku });
+    res.status(200).json({
+      message: "Item retrived",
+      menu: data,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+      error.message = "Something went wrong!";
+    }
+    next(error);
+  }
+};
+
+exports.get = async (req, res, next) => {
+  try {
+    const data = await Menu.find();
+    res.status(200).json({
+      message: "All Menu Retrieved",
+      menu: data
+    })
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+      error.message = "Something went wrong!";
+    }
+    next(error);
+  }
+};
--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const menuController = require('../controller/menuController')
 
 router.post('/add', menuController.add);
+router.get('/get/:foodSku', menuController.getBySku)
+router.get('/get', menuController.get)
 
 module.exports = router;
