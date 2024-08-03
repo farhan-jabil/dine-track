81c600d3072c4a255ccd38f6af192882c6303edb
farhan-jabil <farhanhasan295@gmail.com>
2024-08-03 22:25:47 +0600

    Menu crud complete - backend

--- backend/controller/menuController.js
+++ backend/controller/menuController.js
@@ -38,7 +38,39 @@ exports.get = async (req, res, next) => {
     const data = await Menu.find();
     res.status(200).json({
       message: "All Menu Retrieved",
-      menu: data
+      menu: data,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+      error.message = "Something went wrong!";
+    }
+    next(error);
+  }
+};
+
+exports.edit = async (req, res, next) => {
+  try {
+    const data = req.body;
+    await Menu.updateOne({ foodSku: req.params.foodSku }, { $set: data });
+    res.status(200).json({
+      message: "Item Edit Complete",
+      menu: data,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+      error.message = "Something went wrong!";
+    }
+    next(error);
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    await Menu.findOneAndDelete({ foodSku: req.params.foodSku })
+    res.status(200).json({
+      message: "Item Deleted"
     })
   } catch (error) {
     if (!error.statusCode) {
--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const menuController = require('../controller/menuController')
 
 router.post('/add', menuController.add);
-router.get('/get/:foodSku', menuController.getBySku)
-router.get('/get', menuController.get)
+router.get('/get/:foodSku', menuController.getBySku);
+router.get('/get', menuController.get);
+router.put('/edit/:foodSku', menuController.edit);
+router.delete('/delete/:foodSku', menuController.delete);
 
 module.exports = router;
